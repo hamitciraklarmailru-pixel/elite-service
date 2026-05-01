dbbf165c45d767cd43e475db44be91eee2149264
hamitciraklarmailru-pixel <hamit.ciraklarmail.ru@gmail.com>
2026-05-01 23:38:02 +0300

    Update app.py

--- templates/app.py
+++ templates/app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import os
 
 app = Flask(__name__)
+
+# Google ve diğer arama motorlarının siteyi taramasına izin veren route
+@app.route('/robots.txt')
+def robots():
+    return "User-agent: *\nAllow: /", 200, {'Content-Type': 'text/plain'}
 
 @app.route("/")
 def home():
